0c3cf454bf390e1f49b0e82d1fb3222291a0e2f5
Cathal <92827664+cathal1990@users.noreply.github.com>
2022-11-07 11:33:18 +0000

    Removed unused redux code

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as userSlice } from "../store/users/userSlice";
-export { default as singleUserSlice } from "../store/users/singleUserSlice";
-export { default as loginSlice } from "../store/login/loginSlice";
-
